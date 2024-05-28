d27160fd0699de5deafe8d2ef3c26013e2487c69
zifrit <zifrit@list.ru>
2024-05-28 12:14:31 +0300

    added ProfileStudent, added relationship User with ProfileTeacher and ProfileStudent

--- rambot/base/models/user.py
+++ rambot/base/models/user.py
@@ -1,17 +1,38 @@
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import String, ForeignKey
 
 from base.models import Base
 
 
 class User(Base):
     __tablename__ = "users"
-    username: Mapped[str] = mapped_column(String(255), unique=True)
-    first_name: Mapped[str | None] = mapped_column(String(255))
+    username: Mapped[str] = mapped_column(String(255))
+    tg_id: Mapped[int] = mapped_column(String(255), unique=True)
+    is_student: Mapped[bool]
+    profile_student: Mapped["ProfileTeacher"] = relationship(back_populates="user")
+    is_teacher: Mapped[bool]
+    profile_teacher: Mapped["ProfileTeacher"] = relationship(back_populates="user")
 
 
 class ProfileTeacher(Base):
     __tablename__ = "profiles_teacher"
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        unique=True,
+    )
     first_name: Mapped[str] = mapped_column(String(255))
     last_name: Mapped[str] = mapped_column(String(255))
     middle_name: Mapped[str | None] = mapped_column(String(255))
+    user: Mapped["User"] = relationship(back_populates="profile_teacher")
+
+
+class ProfileStudent(Base):
+    __tablename__ = "profiles_student"
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        unique=True,
+    )
+    first_name: Mapped[str] = mapped_column(String(255))
+    last_name: Mapped[str] = mapped_column(String(255))
+    middle_name: Mapped[str | None] = mapped_column(String(255))
+    user: Mapped["User"] = relationship(back_populates="profile_student")
