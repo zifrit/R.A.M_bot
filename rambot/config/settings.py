0ed535637e3b6286243518df6bcc1e88fa2be18b
zifrit <zifrit@list.ru>
2024-05-30 19:19:21 +0300

    fixed settings

--- rambot/config/settings.py
+++ rambot/config/settings.py
@@ -5,16 +5,12 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
-
-    model_config = SettingsConfigDict(env_file="../.env")
+    BOT_TOKEN: str = ''
 
 
 class DB(BaseSettings):
     DB_URL_FOR_SELECT: str = "sqlite+aiosqlite:///../../db.sqlite3"
     DB_URL_FOR_MIGRATE: str = "sqlite+aiosqlite:///../db.sqlite3"
-
-    model_config = SettingsConfigDict(env_file="../.env")
 
 
 class Postgres(BaseSettings):
@@ -28,8 +24,6 @@ class Postgres(BaseSettings):
     def DATABASE_URL_asyncpg(self):
         # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    model_config = SettingsConfigDict(env_file="../.env")
 
 
 settings = Settings()
