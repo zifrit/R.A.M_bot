11978a7887233c9e12d795b3a67e13496e5dd431
zifrit <zifrit@list.ru>
2024-05-31 11:11:13 +0300

    added tasks and task_types tabel. modified lessons. added link tasks to lessons

--- rambot/base/models/base.py
+++ rambot/base/models/base.py
@@ -1,7 +1,18 @@
+from datetime import datetime
+
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
-from sqlalchemy import String
+from sqlalchemy import String, DateTime
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(),
+        default=datetime.utcnow,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
--- rambot/base/models/lessons.py
+++ rambot/base/models/lessons.py
@@ -1,8 +1,26 @@
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import String, ForeignKey, TEXT
+from sqlalchemy.dialects.postgresql import JSON
 from base.models import Base
 
 
 class Lesson(Base):
     __tablename__ = "lessons"
     name: Mapped[str] = mapped_column(String(255), unique=True)
+    tasks: Mapped["Tasks"] = relationship(back_populates="lessons")
+
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+    task_type_id: Mapped[int] = mapped_column(ForeignKey("task_types.id"))
+    lesson_id: Mapped[int] = mapped_column(ForeignKey("lessons.id"))
+    text: Mapped[str] = mapped_column(TEXT())
+    answer: Mapped[list] = mapped_column(JSON())
+    task_types: Mapped["TasksTypes"] = relationship(back_populates="tasks")
+    lessons: Mapped["TasksTypes"] = relationship(back_populates="tasks")
+
+
+class TasksTypes(Base):
+    __tablename__ = "task_types"
+    name: Mapped[str] = mapped_column(String(255))
+    tasks: Mapped["Tasks"] = relationship(back_populates="task_types")
